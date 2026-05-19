f432999cfd9146584e39081c229def786baf44cd
djorde.rajcic <84-2022@pmf.kg.ac.rs>
2026-05-19 17:27:13 +0200

    Login Page logic and functionalities

--- OrangeHRMQA/src/main/java/org/main/base/BasePage.java
+++ OrangeHRMQA/src/main/java/org/main/base/BasePage.java
@@ -29,6 +29,7 @@ public class BasePage {
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
+
     public boolean isLoaded(WebElement element) {
 
         return explicitWait(element).isDisplayed();
